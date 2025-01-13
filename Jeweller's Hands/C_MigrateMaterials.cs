57220d1254c448121effc376b6f2e9d76b642efb
Ian-Gm <iangromar@gmail.com>
2025-01-13 11:43:54 -0300

    Deloyed version 0.0.39 using another method for material search

--- Jeweller's Hands/C_MigrateMaterials.cs
+++ Jeweller's Hands/C_MigrateMaterials.cs
@@ -21,6 +21,7 @@ using System.Windows.Input;
 using Eto.Forms;
 using Rhino.UI;
 using System.Data.SqlTypes;
+using System.Drawing;
 
 
 namespace JewellersHands
@@ -42,7 +43,7 @@ namespace JewellersHands
 
         protected override Result RunCommand(RhinoDoc doc, RunMode mode)
         {
-            RhinoApp.WriteLine("Material migration routine starts...");
+            RhinoApp.WriteLine("Material migration routine starts... doc.Materials method is being used");
 
             bool flag = false;
 
@@ -59,9 +60,33 @@ namespace JewellersHands
                 return Result.Failure;
             }
 
-            //Material logic
+            /*
 
-            int amountofmaterials = doc.RenderMaterials.Count;
+            BitmapTable BMTable = doc.Bitmaps;
+
+            int amountofbitmaps = BMTable.Count;
+
+            if(amountofbitmaps <= 0)
+            {
+                RhinoApp.WriteLine("There are no bitmaps in this file");
+                return Result.Failure;
+            }
+            else
+            {
+
+                BMTable.ExportToFiles(folderPath, 1);
+                BitmapEntry aBitmap = BMTable.FindIndex(0);
+                aBitmap.
+                for (int i = 0; i < amountofbitmaps; i++)
+                {
+                    Bitmap aBitmap = ;
+                }
+            }
+            */
+
+           
+
+            int amountofmaterials = doc.Materials.Count;
 
             if (amountofmaterials == 0)
             {
@@ -70,23 +95,22 @@ namespace JewellersHands
             }
             else
             {
-                doc.RenderMaterials.BeginChange(RenderContent.ChangeContexts.Script);
+                //doc.RenderMaterials.BeginChange(RenderContent.ChangeContexts.Script);
                 for (int i = 0; i < amountofmaterials; i++)
                 {
-                    RenderMaterial aMaterial = doc.RenderMaterials[i];
-                    aMaterial.BeginChange(RenderContent.ChangeContexts.Script);
-                    RenderTexture aTexture = aMaterial.GetTextureFromUsage(RenderMaterial.StandardChildSlots.Diffuse);
-                    aTexture.BeginChange(RenderContent.ChangeContexts.Script);
-                    string texturefilePath = aTexture.Filename;
-                    string texturefileName = Path.GetFileName(texturefilePath);
+                    RhinoApp.WriteLine(i.ToString() + "- ");
+                    Material aMaterial = doc.Materials[i];
+                    
+                    Texture aTexture = aMaterial.GetBitmapTexture();
 
-                    //string newtexturefilePath = Path.Combine(folderPath, "texture_"+i.ToString()+Path.GetExtension(texturefilePath));
+                    string texturefilePath = aTexture.FileName;
+                    string texturefileName = Path.GetFileName(texturefilePath);
 
                     string newtexturefilePath = Path.Combine(folderPath, texturefileName);
 
                     if (File.Exists(newtexturefilePath))
                     {
-                        RhinoApp.WriteLine(i.ToString() + "- " + texturefileName + " already exists");
+                        RhinoApp.WriteLine(texturefileName + " already exists");
                     }
                     else
                     {
@@ -97,20 +121,48 @@ namespace JewellersHands
                         }
                         catch
                         {
-                            RhinoApp.WriteLine(i.ToString() + "- Couldn't copy " + texturefileName);
+                            RhinoApp.WriteLine("Couldn't copy " + texturefileName);
                             flag = true;
                             continue;
                         }
-                        RhinoApp.WriteLine(i.ToString() + "- " + texturefileName + " was copied to chosen folder");
+                        RhinoApp.WriteLine(texturefileName + " was copied to chosen folder");
                     }
 
-                    aTexture.Filename = newtexturefilePath;
-                    aTexture.EndChange();
-                    aMaterial.EndChange();
+                    Material newMaterial = new Material();
+                    newMaterial.Name = i.ToString();
+                    Texture newTexture = new Texture();
+                    newTexture.FileName = newtexturefilePath;
+                    newMaterial.SetBitmapTexture(newTexture);
+                    newMaterial.CommitChanges();
+
+                    bool changed = doc.Materials.Modify(newMaterial, i, true);
+                    if (!changed)
+                    {
+                        RhinoApp.WriteLine("Couldn't set " + texturefileName + " to material, please do so manually");
+                    }
+                    else
+                    {
+                        RhinoApp.WriteLine(texturefileName + " was set succesfully");
+                    }
+
+                    newMaterial.CommitChanges();
+                    
+                    /*
+                    bool deleted = doc.Materials.Delete(newMaterial);
+
+                    if (deleted)
+                    {
+                        RhinoApp.WriteLine("was deleted");
+                    }
+                    else
+                    {
+                        RhinoApp.WriteLine("could not be deleted");
+                    }*/
+
                 }
             }
 
-            doc.RenderMaterials.EndChange();
+            //doc.RenderMaterials.EndChange();
 
             if (!flag)
             {
@@ -121,6 +173,72 @@ namespace JewellersHands
             {
                 return Result.Failure;
             }
+
+            /*
+           //Material logic
+
+
+           int amountofmaterials = doc.RenderMaterials.Count;
+
+           if (amountofmaterials == 0)
+           {
+               RhinoApp.WriteLine("There are no materials in this file");
+               return Result.Failure;
+           }
+           else
+           {
+               doc.RenderMaterials.BeginChange(RenderContent.ChangeContexts.Script);
+               for (int i = 0; i < amountofmaterials; i++)
+               {
+                   RenderMaterial aMaterial = doc.RenderMaterials[i];
+                   aMaterial.BeginChange(RenderContent.ChangeContexts.Script);
+                   RenderTexture aTexture = aMaterial.GetTextureFromUsage(RenderMaterial.StandardChildSlots.Diffuse);
+                   aTexture.BeginChange(RenderContent.ChangeContexts.Script);
+                   string texturefilePath = aTexture.Filename;
+                   string texturefileName = Path.GetFileName(texturefilePath);
+
+                   //string newtexturefilePath = Path.Combine(folderPath, "texture_"+i.ToString()+Path.GetExtension(texturefilePath));
+
+                   string newtexturefilePath = Path.Combine(folderPath, texturefileName);
+
+                   if (File.Exists(newtexturefilePath))
+                   {
+                       RhinoApp.WriteLine(i.ToString() + "- " + texturefileName + " already exists");
+                   }
+                   else
+                   {
+                       try
+                       {
+                           File.Copy(texturefilePath, newtexturefilePath, false);
+
+                       }
+                       catch
+                       {
+                           RhinoApp.WriteLine(i.ToString() + "- Couldn't copy " + texturefileName);
+                           flag = true;
+                           continue;
+                       }
+                       RhinoApp.WriteLine(i.ToString() + "- " + texturefileName + " was copied to chosen folder");
+                   }
+
+                   aTexture.Filename = newtexturefilePath;
+                   aTexture.EndChange();
+                   aMaterial.EndChange();
+               }
+           }
+
+           doc.RenderMaterials.EndChange();
+
+           if (!flag)
+           {
+               RhinoApp.WriteLine("Material migration was succesful");
+               return Result.Success;
+           }
+           else
+           {
+               return Result.Failure;
+           }
+           */
         }
     }
 }
